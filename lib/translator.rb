bdde4092dce4c68afc057a2f7d29f60917b64b62
Ruben Vallejo <r.will16@hotmail.com>
2019-09-30 19:13:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,7 +21,7 @@ def get_japanese_emoticon(file_path, emoticon)
   if emoticon
   translation_hash["get_emoticon"][emoticon]
 else
-  return "I am sorry but the emoticon entered is not known"
+  return "Sorry, that emoticon was not found"
 end
 end
 
