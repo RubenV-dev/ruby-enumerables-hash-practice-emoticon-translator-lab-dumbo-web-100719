90b82ee969827765e33b648f51bfb8f139788c0b
Ruben Vallejo <r.will16@hotmail.com>
2019-09-30 18:43:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,12 +4,12 @@ require "yaml"
 def load_library(file_path)
   emoticon_hash = YAML.load_file(file_path)
   returned_hash = {
-    "get_meaning" => {},
-    "get_emoticon" => {}
+    "get_meaning" = {},
+    "get_emoticon" = {}
   }
     emoticon_hash.each do |emoticon_name, (english,japanese)|
-      returned_hash[get_meaning].merge("english" => {})
-      returned_hash[get_emoticon].merge("japanese" => {})
+      returned_hash[get_meaning].merge("english" = {})
+      returned_hash[get_emoticon].merge("japanese" = {})
     end
     returned_hash
     p returned_hash
